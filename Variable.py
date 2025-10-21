0af9a1ae36f7eba26b5d1b8079245f0d74890cf3
unknown <tijanifathia433@gmail.com>
2025-10-21 06:23:23 +0100

    introduction

---
+++ Variable.py
@@ -0,0 +1,86 @@
+name = "Kamaldeen"
+print(name)
+
+fabrics = "silk"
+print(fabrics)
+
+age = 40
+print(age)
+
+number = 6
+print(number)
+
+me = "kamaldeen"
+print(me)
+
+
+num = 7
+print(num)
+
+network = "MTN"
+
+print(network)
+
+# to print my gender 
+gender = "female"
+print(gender)
+
+
+# to print my name
+name1 = "kamaldeen"
+print(name1)
+
+
+# to print my second name
+name2 = "bello"
+print(name2)
+
+#CASTING
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+
+print(x)
+print(y)
+print(z)
+
+# Case Sentivity
+a = 4
+A = "Sally"
+# A will not overwrite a
+
+print(a)
+print(A)
+
+age = 10
+Age = 12
+ 
+print(Age)
+
+
+i = str(5)
+f = int(10)
+k = float(13)
+
+print(type(i))
+print(type(f))
+print(type(k))
+
+g = "5"
+m = 5
+o = 5.5
+
+
+print(type(g))
+print(type(m))
+print(type(o))
+
+
+
+p = str (5) 
+q = int (4)
+w = float(7)
+
+print(type(p))
+print(type(q))
+print(type(w))
---
+++ syntax.py
@@ -0,0 +1,51 @@
+print("Hello World!")
+
+# indentation
+if 5 > 2:
+    print("Five is greater than two!")
+
+# statement
+print("Hello World!")
+print("Have a good day.")
+print("Learning Python is fun!")
+
+print("im learning python")
+print("im new here")
+print("my name is kamaldeen im new here")
+
+print("im a doctor")
+print("new doctor")
+
+print("name")
+print("age")
+print('you is me')
+print('13')
+
+# text Vs number
+print("name")
+print(40)
+
+print("I am", 35, "years old.")
+print(3 + 3)
+print(2 * 5)
+
+# comment is use with an hastag
+# we did syntax
+# we did output
+
+# This is a comment
+# written in
+# more than just one line
+print("Hello, World!")
+
+# multiple comments
+# it is assign by 3 quotes
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Hello, World!")
+
+print("kamaldeen")  # this is my name
+
